6510bf2cabe449a182d554ae90e5dc0fe3cbd74a
Eric Mignot <eric.mignot@gmail.com>
2013-11-03 10:14:42 -0500

    UnusedCode removed

--- Yose/Global.asax.cs
+++ Yose/Global.asax.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections;
-using System.ComponentModel;
-using System.Web;
-using System.Web.SessionState;
 using ServiceStack.WebHost.Endpoints;
 
 namespace Yose
@@ -11,49 +7,18 @@ namespace Yose
     {
         public class HelloAppHost : AppHostBase
         {
-            //Tell Service Stack the name of your application and where to find your web services
-            public HelloAppHost() : base("Hello Web Services", typeof(HelloService).Assembly)
+            public HelloAppHost() : base("Yose Services", typeof(PingService).Assembly)
             {
             }
 
             public override void Configure(Funq.Container container)
             {
-                //register any dependencies your services use, e.g:
-                //container.Register<ICacheClient>(new MemoryCacheClient());
             }
         }
 
         protected void Application_Start(Object sender, EventArgs e)
         {
             new HelloAppHost().Init();
-        }
-
-        protected void Session_Start(Object sender, EventArgs e)
-        {
-        }
-
-        protected void Application_BeginRequest(Object sender, EventArgs e)
-        {
-        }
-
-        protected void Application_EndRequest(Object sender, EventArgs e)
-        {
-        }
-
-        protected void Application_AuthenticateRequest(Object sender, EventArgs e)
-        {
-        }
-
-        protected void Application_Error(Object sender, EventArgs e)
-        {
-        }
-
-        protected void Session_End(Object sender, EventArgs e)
-        {
-        }
-
-        protected void Application_End(Object sender, EventArgs e)
-        {
         }
     }
 }
